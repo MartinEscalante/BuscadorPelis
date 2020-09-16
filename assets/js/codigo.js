a48a1e3691505793912e854d8845134ad24f6912
Hugo D. Morales <60529414+dam788@users.noreply.github.com>
2020-09-15 21:32:09 -0300

    primer repo de pelis

---
+++ assets/js/codigo.js
@@ -0,0 +1,13 @@
+let input = document.querySelector('#idInput');
+const llamarPeli = ({target}) => {
+
+    let = target.value
+
+
+    // fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=7e849978&s=${}`)
+
+
+
+}
+input.addEventListener('input', llamarPeli)
+
